c59b60566bb2dad09a3f0077541c684fc4aefddc
meloa2atwit <meloa2@wit.edu>
2023-12-03 18:17:54 -0500

    Started working on main.py

--- codes/main.py
+++ codes/main.py
@@ -13,11 +13,6 @@ df = pd.read_csv(filepath_or_buffer=DS)
 # Split data set to train and test data
 train_size = int(df.shape[0] * 0.8)
 test_size = int(df.shape[0] - train_size)
-train_df = df.sample(n=train_size, frac=None, replace=False, random_state=RANDOM_SEED,weights=None, axis="index")
-
+train_df = df.sample(n=train_size, frac=None, replace=False, random_state=RANDOM_SEED, weights=None, axis="index")
 
 ## TESTING
-cols = train_df.columns
-for idx, c in enumerate(cols):
-    value = train_df.at[0, c]
-    print(f"| {c} | {value} |")
